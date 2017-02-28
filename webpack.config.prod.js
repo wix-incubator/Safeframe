9a3cae1262294eeda628423a89c51c7a2dc97f6a
Djordje Ungar <djordje.ungar@gmail.com>
2017-02-28 14:53:30 +0100

    Add version on top of the generated files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,7 @@
+var webpack = require("webpack");
 var path = require('path');
 var secretKey = require('fs').readFileSync(path.join(__dirname, 'secret.key'));
+var version = require("./package.json").version;
 
 module.exports = {
     entry: {
@@ -16,5 +18,9 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader?presets[]=es2015!preprocess-loader?-DEBUG&SECRET_KEY='+secretKey+'&NODE_ENV=production'
         }]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin("@generated"),
+        new webpack.BannerPlugin("DeviantArt SafeFrame v" + version),
+    ],
 };
